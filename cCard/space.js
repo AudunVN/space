7b9bf5e51ca7d5f1bd1b51bf417471c6aff1f377
Space <space@471.no>
2017-02-24 14:52:34 +0100

    ul new contact card design (v1)

---
+++ cCard/space.js
@@ -0,0 +1,48 @@
+var starClassArray = ["O", "B", "A", "F", "F", "F", "G", "G", "G", "G", "G", "G", "G", "K", "K", "K", "K", "K", "K", "K", "K", "K", "K", "K", "K", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "M"];
+
+function generateStarfield(starCount, containerSelector, avgFreq) {
+	for (i = 0; i < starCount; i++) {
+		x = Math.random() * 100;
+		y = Math.random() * 100;
+		magnitude = Math.random();
+		starClass = starClassArray[Math.floor(Math.random() * starClassArray.length)];
+		new Star(x, y, magnitude, starClass, i, containerSelector);
+	}
+	twinkleEvent(avgFreq, starCount);
+}
+
+function twinkleEvent(avgFreq, elementCount) {
+	if (!document.querySelector("body.scrolling")) {
+		// -> removing the class
+		var element = document.querySelector("#no" + Math.floor(Math.random() * elementCount));
+		element.classList.remove("twinkle");
+
+		// -> triggering reflow /* The actual magic */
+		// without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+		element.offsetWidth = element.offsetWidth;
+
+		// -> and re-adding the class
+		element.classList.add("twinkle");
+	}
+	setTimeout(function() {
+		twinkleEvent(avgFreq, elementCount);
+	}, Math.random() * 1000 + 1000 / avgFreq);
+}
+
+function Star(x, y, magnitude, classString, number, containerSelector) {
+	var starObject = document.createElement("div");
+	var container = document.querySelector(containerSelector);
+	starObject.className = "star " + classString;
+	starObject.id = "no" + number;
+	starObject.style.left = x + "%";
+	starObject.style.top = y + "%";
+
+	starObject.style.opacity = magnitude;
+
+	starObject.style.position = "absolute"; 		
+	container.appendChild(starObject);
+}
+
+(function init() {
+	generateStarfield(200, '.starfield', 500000);
+})();
